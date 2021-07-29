2ac3d695f54d97a9d2d7f018bfd2a7475d88ca51
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-28 22:40:52 -0400

    added router to filter users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,9 @@ const Connection = require('./models/Connections.js');
 const Genre = require('../models/Genre');
 const Instrument = require('../models/Instrument');
 
-//== returning all users with user info except password ==//
+
+
+//==== returning all users with user info except password ====//
 router.get('/api/user', async (req, res) => {
   const users = await User.findAll({ attributes: { exclude: ['password'] } });
 
@@ -18,27 +20,48 @@ router.get('/api/user', async (req, res) => {
   res.send({ users: users });
 });
 
-//== returning users by id ==//
+//==== returning users by id ====//
 router.get('/user/:id', async (req, res) => {
   const user = await User.findByPk(req.params.id, { attributes: { exclude: ['password'] } })
   if (!user) {
     res
       .status(500)
-      .json({ message: 'Error getting users' });
+      .json({ message: 'Error getting user id' });
     return;
   }
 
   res.send(user);
 });
 
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+//==== filter userData ====//
+router.get('/api/connections', async (req, res) => {
+  const userData = await User.findAll(req.body, { attributes: { exclude: ['password'] } })
+
+  if (!userData) {
+    res
+      .status(500)
+      .json({ message: 'Error getting user data' });
+    return;
+  }
+
+  res.send(userData);
+});
+
+//==== GET /API/Connections/:user_id -user_id = target_id ====//
+
+
+
+
+
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
