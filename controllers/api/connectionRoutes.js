36cc3a998ca39bd4754087dd2a5f5af7614c867b
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-29 14:41:06 -0400

    created routes for delete

--- controllers/api/connectionRoutes.js
+++ controllers/api/connectionRoutes.js
@@ -1,49 +1,55 @@
 const router = require('express').Router();
-const User  = require('../../models');
+const User = require('../../models');
 const Connection = require('./models/Connections.js');
 const Genre = require('../models/Genre');
 const Instrument = require('../models/Instrument');
 
 //==== find user connections ====//
 router.get('/api/connections/user/:id', async (req, res) => {
-    const connections = await Connection.findAll({where: {user_id: req.params.id}});
-  
+    const connections = await Connection.findAll({ where: { user_id: req.params.id } });
+
     if (!connections) {
-      res
-        .status(500)
-        .json({ message: 'Error getting connections' });
-      return;
+        res
+            .status(500)
+            .json({ message: 'Error getting connections' });
+        return;
     }
-  
+
     res.send(connections);
-  });
-
-
-  //==== Post user connections ====//
-  //POST /API/Connection/user_id&:target_id
-
-  router.post('/api/connections/user_id/:target_id', async (req, res) => {
-    const connections = await Connection.findAll({where: {user_id: req.params.id}});
-  
-    if (!connections) {
-      res
-        .status(500)
-        .json({ message: 'Error getting connections target id' });
-      return;
-    }
-    res.send(connections);
-  });
-
-
-//==== DELETE /API/Connections/:target_id - on rejection ====//
-router.delete('api/connections/:target_id', (req, res) => {
-    const { username } = req.params;
-    db.collection('username').findOneAndDelete({username: username}, 
-    (err, result) => {
-    if (err) return res.send(500, err)
-    console.log('got deleted');
-    res.redirect('/');
-    });
 });
 
-//==== DELETE /API/Connections/:user_id ====//
+
+//==== post user connections ====//
+router.post('/api/connections/make_connection/', async (req, res) => {
+    let { userID, targetID } = request.body
+    try {
+        const connections = await Connection.create({ user_id: userID, target_id: targetID });
+        res.status(200).json(connections);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+//==== delete user connections ====//
+router.delete('api/connections/', async (req, res) => {
+    let { user_id } = req.body;
+    try {
+        const connections = await Connection.destroy({ where: { user_id: user_id } });
+        const connections_targetid = await Connection.destroy({ where: { target_id: user_id } });
+        res.status(200).json(connections, connections_targetid);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//====  rejecting a connection request ====//
+router.delete('api/connections/:connection_id', (req, res) => {
+    const { connection_id } = req.params;
+    try {
+        const connections = await Connection.destroy({ where: { id: connection_id } });
+        res.status(200).json(connections);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
