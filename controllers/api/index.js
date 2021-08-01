52a1718789424de8bb6a0164473528ae40e50e54
kyle sexton <ksexton.exe@gmail.com>
2021-08-01 18:36:26 -0400

    updated connections route to matches, removed extra files, routes working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const connectionRoutes = require('./connectionRoutes');
 const matchesRoutes = require('./matchesRoutes');
 
 router.use('/matches', matchesRoutes);
 router.use('/users', userRoutes);
-router.use('/connections', connectionRoutes);
 // router.use('/projects', projectRoutes);
 
 module.exports = router;
--- controllers/api/matchesRoutes.js
+++ controllers/api/matchesRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { Connections } = require('../../models');
+const { Sequelize } = require('sequelize');
+const { Connections, User } = require('../../models');
 
+//get a connection by id
 router.get('/', async (req, res) => {
   let { id } = req.query;
   try {
@@ -18,17 +20,70 @@ router.get('/', async (req, res) => {
   }
 });
 
+//get a user's connections by id
+router.get('/getConnections', async (req, res) => {
+  let { id } = req.query;
+  try {
+    const userData = await User.findByPk(id, {
+      attributes: ['connectionsList'],
+    });
+    res.status(200).json(userData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
 //==== user connects with somebody else ====//
 router.post('/make_connection', async (req, res) => {
   let { user_id, target_id } = req.body;
   try {
+    //make connection
     const connections = await Connections.create({
       user_id: user_id,
       target_id: target_id,
     });
+    //update user, need connections to update
+    const updateUser = await User.update(
+      {
+        connectionsList: Sequelize.fn(
+          'CONCAT',
+          Sequelize.col('connections_list'),
+          ';' + connections.id.toString()
+        ),
+      },
+      { where: { id: user_id } }
+    );
+    //update target
+    const updateTarget = await User.update(
+      {
+        connectionsList: Sequelize.fn(
+          'CONCAT',
+          Sequelize.col('connections_list'),
+          ';' + connections.id.toString()
+        ),
+      },
+      { where: { id: target_id } }
+    );
     res.status(200).json(connections);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+//resolve connection by id
+router.put('/resolve', async (req, res) => {
+  let { id } = req.query;
+  try {
+    const con = await Connections.update(
+      { accepted: true },
+      {
+        where: { id: id },
+      }
+    );
+    res.status(200).json(con);
+  } catch (error) {
+    res.status(500).json({ message: 'Error getting connections', error });
   }
 });
 
